d104ecc21f72df101a7b05aea9fe28a4ca3a0652
Phantom <71116566+jishnukarri@users.noreply.github.com>
2025-03-16 19:33:38 +0000

    Update build.py

--- build.py
+++ build.py
@@ -3,7 +3,8 @@ import PyInstaller.__main__
 
 # Configuration
 SCRIPT_NAME = "bot.py"  # Replace with your main script file
-ICON_PATH = r"icon.ico"  # Replace with the path to your icon file (optional)
+ICON_PATH_BACKGROUND = "build/background/icon.ico"  # Path to icon in background directory
+ICON_PATH_CONSOLE = "build/console/icon.ico"  # Path to icon in console directory
 
 # Output directories
 OUTPUT_DIR_BACKGROUND = "build/background"
@@ -21,8 +22,7 @@ PyInstaller.__main__.run([
     '--distpath', OUTPUT_DIR_BACKGROUND,
     '--workpath', os.path.join(OUTPUT_DIR_BACKGROUND, 'temp'),
     '--specpath', OUTPUT_DIR_BACKGROUND,
-    '--icon', ICON_PATH,  # Optional: Add an icon
-    '--name', 'bot',  # Name the output file (will have .exe extension on Windows)
+    '--icon', ICON_PATH_BACKGROUND,  # Use icon in background directory
     SCRIPT_NAME
 ])
 
@@ -33,8 +33,7 @@ PyInstaller.__main__.run([
     '--distpath', OUTPUT_DIR_CONSOLE,
     '--workpath', os.path.join(OUTPUT_DIR_CONSOLE, 'temp'),
     '--specpath', OUTPUT_DIR_CONSOLE,
-    '--icon', ICON_PATH,  # Optional: Add an icon
-    '--name', 'bot',  # Name the output file (will have .exe extension on Windows)
+    '--icon', ICON_PATH_CONSOLE,  # Use icon in console directory
     SCRIPT_NAME
 ])
 
